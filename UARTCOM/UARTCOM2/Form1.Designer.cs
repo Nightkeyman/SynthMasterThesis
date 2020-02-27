40f705f22437ae25e0ff8e4f2222a4042b0450ee
waciarz <jpelka2@gmail.com>
2020-02-28 00:04:52 +0100

    Com Port C# app

---
+++ UARTCOM/UARTCOM2/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace UARTCOM2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetSerialPorts = new System.Windows.Forms.Button();
+            this.comboBox_port = new System.Windows.Forms.ComboBox();
+            this.textBox_baudrate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_open = new System.Windows.Forms.Button();
+            this.richTextBox_received = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.textBox_send = new System.Windows.Forms.TextBox();
+            this.button_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetSerialPorts
+            // 
+            this.btnGetSerialPorts.Location = new System.Drawing.Point(202, 34);
+            this.btnGetSerialPorts.Name = "btnGetSerialPorts";
+            this.btnGetSerialPorts.Size = new System.Drawing.Size(75, 23);
+            this.btnGetSerialPorts.TabIndex = 0;
+            this.btnGetSerialPorts.Text = "Refresh";
+            this.btnGetSerialPorts.UseVisualStyleBackColor = true;
+            this.btnGetSerialPorts.Click += new System.EventHandler(this.btnGetSerialPorts_Click);
+            // 
+            // comboBox_port
+            // 
+            this.comboBox_port.FormattingEnabled = true;
+            this.comboBox_port.Location = new System.Drawing.Point(96, 36);
+            this.comboBox_port.Name = "comboBox_port";
+            this.comboBox_port.Size = new System.Drawing.Size(100, 21);
+            this.comboBox_port.TabIndex = 2;
+            // 
+            // textBox_baudrate
+            // 
+            this.textBox_baudrate.Location = new System.Drawing.Point(96, 63);
+            this.textBox_baudrate.Name = "textBox_baudrate";
+            this.textBox_baudrate.Size = new System.Drawing.Size(100, 20);
+            this.textBox_baudrate.TabIndex = 3;
+            this.textBox_baudrate.Text = "115200";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Baudrate:";
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(96, 90);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(100, 23);
+            this.button_open.TabIndex = 6;
+            this.button_open.Text = "Open";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // richTextBox_received
+            // 
+            this.richTextBox_received.Location = new System.Drawing.Point(400, 34);
+            this.richTextBox_received.Name = "richTextBox_received";
+            this.richTextBox_received.Size = new System.Drawing.Size(225, 175);
+            this.richTextBox_received.TabIndex = 7;
+            this.richTextBox_received.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(397, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Received:";
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(550, 8);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(75, 23);
+            this.button_clear.TabIndex = 9;
+            this.button_clear.Text = "Clear";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // textBox_send
+            // 
+            this.textBox_send.Location = new System.Drawing.Point(400, 215);
+            this.textBox_send.Name = "textBox_send";
+            this.textBox_send.Size = new System.Drawing.Size(144, 20);
+            this.textBox_send.TabIndex = 10;
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(550, 215);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(75, 23);
+            this.button_send.TabIndex = 11;
+            this.button_send.Text = "Send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.textBox_send);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox_received);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_baudrate);
+            this.Controls.Add(this.comboBox_port);
+            this.Controls.Add(this.btnGetSerialPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetSerialPorts;
+        private System.Windows.Forms.ComboBox comboBox_port;
+        private System.Windows.Forms.TextBox textBox_baudrate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.RichTextBox richTextBox_received;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.TextBox textBox_send;
+        private System.Windows.Forms.Button button_send;
+    }
+}
+
---
+++ UARTCOM/UARTCOM2/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace UARTCOM2
+{
+    
+    public partial class Form1 : Form
+    {
+        SerialPort ComPort = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+            ComPort.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+        }
+
+        private void btnGetSerialPorts_Click(object sender, EventArgs e)
+        {
+            string[] ArrayComPortsNames = null;
+            int index = -1;
+            string ComPortName = null;
+
+            ArrayComPortsNames = SerialPort.GetPortNames();
+            do
+            {
+                index += 1;
+                comboBox_port.Items.Add(ArrayComPortsNames[index]);
+            }
+
+            while (!((ArrayComPortsNames[index] == ComPortName)
+                          || (index == ArrayComPortsNames.GetUpperBound(0))));
+            Array.Sort(ArrayComPortsNames);
+
+            //want to get first out
+            if (index == ArrayComPortsNames.GetUpperBound(0))
+            {
+                ComPortName = ArrayComPortsNames[0];
+            }
+            comboBox_port.Text = ArrayComPortsNames[0];
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            if (button_open.Text.Equals("Open"))
+            {
+                ComPort.BaudRate = Convert.ToInt32(textBox_baudrate.Text);
+                ComPort.PortName = comboBox_port.Text;
+                ComPort.Open();
+                
+                button_open.Text = "Close";
+            } else
+            {
+               
+                ComPort.Close();
+                button_open.Text = "Open";
+            }
+               
+        }
+        delegate void SetTextCallback(string text);
+        private void setText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.richTextBox_received.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(setText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.richTextBox_received.Text = text;
+            }
+        }
+        delegate string GetTextCallback();
+        private string getText()
+        {
+            string ret = "";
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.richTextBox_received.InvokeRequired)
+            {
+                GetTextCallback d = new GetTextCallback(getText);
+                ret = (string)this.Invoke(d);
+            }
+            else
+            {
+                ret = this.richTextBox_received.Text;
+            }
+            return ret;
+        }
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            while (ComPort.BytesToRead > 0)
+            {
+                int x = ComPort.ReadByte();
+                Console.WriteLine(x);
+               
+                setText( getText() +  x.ToString("X2") + " " );
+                
+            }
+                
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            setText("");
+        }
+
+        private void button_send_Click(object sender, EventArgs e)
+        {
+            if (ComPort.IsOpen)
+            {
+                byte[] bytes = textBox_send.Text.Split(' ').Select(s => Convert.ToByte(s, 16)).ToArray();
+                ComPort.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
