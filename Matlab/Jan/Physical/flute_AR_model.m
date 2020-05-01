7259cbb9ad3a3f22e306aafe64408e7de4d16181
Jan Sadlek <nightkeyman@gmail.com>
2020-05-01 16:40:00 +0200

    JS: Initial flute ARMA model

--- Matlab/Jan/Physical/flute_AR_model.m
+++ Matlab/Jan/Physical/flute_AR_model.m
@@ -3,7 +3,7 @@ clear
 Fs = 44100;
 f = 1:2048;
 fft_size = 2048;
-y = audioread("Samples/flute_A_2.wav");
+y = audioread('Samples/flute_A_2.wav');
 
 signal = y(1:fft_size, 1);
 hann_coef = hanning(fft_size);
@@ -16,14 +16,16 @@ figure(4);
 f = ((f-1)/fft_size)*Fs;
 plot(f,spect_dens);
 axis([0 4000 -40 40])
-title("Flute sound spectral density");
+title('Flute sound spectral density');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 f = 0:0.1:round(Fs/2);
-T = 2;
+T = 3;
 s = [1 zeros(1,length(0:1/Fs:T-1/Fs))];
-outsig = noise(length(0:1/Fs:T-1/Fs), 1, 'white');
+insig = randn(length(0:1/Fs:T-1/Fs), 1);
+insig = ADSR(insig,5,2,2,2);
+%outsig = noise(length(0:1/Fs:T-1/Fs), 1, 'white');
 Resonances_freq_A = [440 880 1312 1764 2194 2649 3513];
 Resonances_mag_A = [33.6 30.58 18.289 22.35 4.53 0.67 -4.06];
 Resonances_mag_A = 100.^(Resonances_mag_A/20);
@@ -33,8 +35,6 @@ for i=1:2:length(Apoles)
     Apoles(i+1) = (1 - 0.05/Resonances_mag_A(fix((i+1)/2)))*exp(-1i*Resonances_freq_A((i+1)/2)*2*pi/Fs);
 end
 A = poly(Apoles);
-
-
 
 Resonances_freq_B = [660 1096 1538 1979 2421 3081];
 Resonances_mag_B = [33.6 30.58 18.289 22.35 4.53 0.67];
@@ -50,7 +50,6 @@ B = [0 0 poly(Bzeros)];
 
 figure(1);
 zplane(B, A);
-% A = [1 -2.7607 3.8106 -2.6535 0.9238];
 
 [H,F] = freqz(B,A,f,Fs);
 figure(2)
@@ -60,9 +59,9 @@ xlabel('Frequency (Hz)')
 ylabel('PSD (dB/Hz)')
 axis([0 5000 0 100]);
 
-abs(Apoles)
-atan(imag(Apoles)./real(Apoles))*Fs/(2*pi)
-Y = filter(B, A, outsig);
+abs(Apoles);
+atan(imag(Apoles)./real(Apoles))*Fs/(2*pi);
+Y = filter(B, A, insig);
 
 figure(3)
 y = impz(1, A);
