c95f81e6e923b6ea0fd366c84254139318ef0c3c
waciarz <jpelka2@gmail.com>
2020-07-25 11:29:42 +0200

    edited

---
+++ workspPADK_MIDI_CCS6_original/workspPADK_MIDI_CCS6/MIDI/Release/filters.pp
@@ -0,0 +1,25 @@
+# FIXED
+
+filters.obj: ../filters.c
+filters.obj: ../filters.h
+filters.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/math.h
+filters.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/float.h
+filters.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/access.h
+filters.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/linkage.h
+filters.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/elfnames.h
+filters.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/mathf.h
+filters.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/unaccess.h
+filters.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/mathl.h
+filters.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/unaccess.h
+
+../filters.c: 
+../filters.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/math.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/float.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/access.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/elfnames.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/mathf.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/unaccess.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/mathl.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/unaccess.h: 
---
+++ workspPADK_MIDI_CCS6_original/workspPADK_MIDI_CCS6/MIDI/Release/midi_fifo.pp
@@ -0,0 +1,7 @@
+# FIXED
+
+midi_fifo.obj: ../midi_fifo.c
+midi_fifo.obj: ../midi_fifo.h
+
+../midi_fifo.c: 
+../midi_fifo.h: 
